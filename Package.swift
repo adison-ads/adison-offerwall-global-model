68087fb378ad6f67243f8be5432ae4304b9d1b3b
Jaehyeon Kim <kim.jaehyeon@nbt.com>
2026-08-26 00:05:59 +0900

    공통 모듈 의존성을 exact에서 from 범위로 변경
    국내/글로벌 SDK를 동시 탑재한 매체에서 공통 모듈의 요구 버전이 어긋나면
    의존성 해석이 실패하므로, 두 릴리즈 트레인이 공유하는 공통 모듈은 범위로
    선언해 더 높은 요구 버전으로 정렬되도록 한다.
    
    floor(1.1.0)는 웹 바이너리가 실제로 요구하는 최소 버전이며,
    ABI 안정성이 깨지는 순간일 때만 직접 올린다.

--- Package.swift
+++ Package.swift
@@ -5,7 +5,6 @@ import PackageDescription
 
 let version = "1.2.0"
 let checksum = "3c0d54f241ab131f033bcb978f94238c0e50ffb24b9d386451367dbc696b0563"
-let commonVersion = "1.1.4"
 
 let package = Package(
     name: "AdisonOfferwallGlobalModel",
@@ -15,7 +14,7 @@ let package = Package(
             targets: ["AdisonOfferwallGlobalModel", "AdisonOfferwallGlobalModelTarget"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/adison-ads/adison-offerwall-common", exact: Version(stringLiteral: commonVersion))
+        .package(url: "https://github.com/adison-ads/adison-offerwall-common", from: "1.1.4")
     ],
     targets: [
         .binaryTarget(
